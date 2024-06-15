997d3aa23a4a076ec1e21f8cd6c67aebdbd864f3
akagrawal <arpitagr.2011@gmail.com>
2024-06-15 14:08:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(100), nullable=False)
+    model = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.String(4), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/list-vehicle', methods=['GET', 'POST'])
+@login_required
+def list_vehicle():
+    if request.method == 'POST':
+        vehicle = Vehicle(
+            make=request.form['make'],
+            model=request.form['model'],
+            year=request.form['year'],
+            price=request.form['price'],
+            owner_id=current_user.id
+        )
+        db.session.add(vehicle)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('list_vehicle.html')
+
+@app.route('/search-vehicles', methods=['GET'])
+def search_vehicles():
+    vehicles = Vehicle.query.all()
+    return render_template('search_vehicles.html', vehicles=vehicles)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
